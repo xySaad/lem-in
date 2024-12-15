4692e7b104dc9a7c736b63615e895d941b385847
srm <saadrm.dev@gmail.com>
2024-12-15 12:09:02 +0100

    fix: check for unexisted rooms in links

--- utils/parser/room_links.go
+++ utils/parser/room_links.go
@@ -4,12 +4,15 @@ func (af *antFarm) parseRoomLinks() error {
 	for i, char := range af.currentLine {
 		if char == '-' {
 			room, exists := af.rooms[af.currentLine[:i]]
-			if exists {
-				_, exists := af.rooms[af.currentLine[i+1:]]
-				if exists {
-					room.links = append(room.links, af.currentLine[i+1:])
-				}
+			if !exists {
+				return af.ParsingError("can't link unexisted room", 0)
 			}
+			_, exists = af.rooms[af.currentLine[i+1:]]
+			if !exists {
+				return af.ParsingError("can't link unexisted room", 0)
+			}
+			room.links = append(room.links, af.currentLine[i+1:])
+
 		}
 	}
 	return nil
